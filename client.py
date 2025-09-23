a23d8382384d9e17ea957f00adf824c0e867f8dc
David Borland <borland@renci.org>
2025-09-23 19:26:04 -0400

    Showing sources for bdc

--- client.py
+++ client.py
@@ -27,11 +27,11 @@ with open( "style.css" ) as css:
 bot_icon = "static/bot-32x32.png"
 user_icon = "static/user-32x32.png"
 
-
+# Set the server to use, local or remote
 #default_rag_chain = RemoteRunnable(url="http://localhost:8000/bdc-bot")
 default_rag_chain = RemoteRunnable(url="https://bdcbot-s.apps.renci.org/bdc-bot")
 
-
+# Dict for source document types
 doc_type_dict = defaultdict(lambda: "Source")
 doc_type_dict['page'] = "BDC Web Page"
 doc_type_dict['docs'] = "BDC Documentation"
@@ -39,13 +39,14 @@ doc_type_dict['update'] = "BDC Update"
 doc_type_dict['event'] = "BDC Event"
 doc_type_dict['faq'] = "BDC FAQ"
 
-# Initialize D3 graph
+# Initialize D3 graph for knowledge graph visualization
 d3 = d3graph(support=None)
 
 def filter_sources(docs):
     # Split by the maximum distance between scores
     # XXX: Could use something more sophisticated such as Otsu thresholding...
     
+    # Just returning all for now
     return docs
     
     # sort docs by score
@@ -66,17 +67,18 @@ def filter_sources(docs):
 
     return top_docs
 
-
-def parse_context(context):
-    docs = context
-
-    if not docs:
+# Parse bdc documentation context into sources
+def parse_bdc_context(context):
+    if not context:
         return []
-    
-    sources = []    
+  
+    docs = []  
+    for doc in context:
+        docs.append(doc.dict())  
     
     top_docs = filter_sources(docs)
     
+    sources = []  
     for doc in top_docs:
         url = ""
 
@@ -114,9 +116,7 @@ def parse_context(context):
 
     return sources
 
-def source_link(url, title, type):
-    return st.text(f"[{type}] {title}")
-
+# Order to display document types
 doc_type_order = [
     "faq",
     "docs",
@@ -125,6 +125,7 @@ doc_type_order = [
     "event"
 ]
 
+# Draw BDC documentation sources
 def draw_sources(sources, showSources):
     if not sources:
         return
@@ -150,6 +151,7 @@ def draw_sources(sources, showSources):
                 # Join lines with a line break and render via markdown
                 st.markdown("<br>".join(source_lines), unsafe_allow_html=True)
 
+# Color for knowledge graph nodes
 def string_to_color(s):
     # Simple hash to color hex (for demonstration)
     import hashlib
@@ -157,6 +159,7 @@ def string_to_color(s):
         s = "default"
     return '#' + hashlib.md5(s.encode()).hexdigest()[:6]
 
+# Process the dug knowledge graph for visualization
 def process_kg(kg):
     # Check if kg has no nodes or edges before displaying
     if not kg or 'nodes' not in kg or 'edges' not in kg:
@@ -198,6 +201,7 @@ def process_kg(kg):
 
     return adjmat, df
 
+
 def draw_additional_response(response, response_title, show_response, kg=None):
     with st.expander(response_title, expanded=show_response):
         st.markdown(response)
@@ -213,26 +217,9 @@ def draw_additional_response(response, response_title, show_response, kg=None):
             d3.graph(adjmat)
             d3.set_node_properties(label=df['label'].values, color=df['color'].values)
             d3.show(show_slider=False, save_button=False)
-                
+
+# Set the current chain to use to get response from server
 current_chain = default_rag_chain
-
-
-
-def format_predefined_response(predefined_response_list, predefined_context, main_response):
-    predefined_response = ""
-    if predefined_response_list:
-        for i, predefined_response in enumerate(predefined_response_list):
-            predefined_response += f"{predefined_context[i]["topic"]} response: {predefined_response}\n"
-    else:
-        predefined_response = "No predefined response found"
-    
-    
-    
-    if main_response:
-        predefined_response = f"Main response: {main_response}\n\n {predefined_response}"
-
-    return predefined_response
-
 
 #with st.sidebar:
 #    st.header("BDC Resources")
@@ -240,6 +227,7 @@ def format_predefined_response(predefined_response_list, predefined_context, mai
 #    st.link_button("Documentation", "https://bdcatalyst.gitbook.io/", icon="📖", use_container_width=True)
 #    st.link_button("Support", "https://bdcatalyst.freshdesk.com/", icon="🛟", use_container_width=True)
 
+# Introduction text
 introduction = """
 This is a prototype of the BDCBot in development at RENCI.
 If you are a tester, please complete [this form](https://example.com) after your test.
@@ -256,14 +244,11 @@ st.text('[BDCBOT TEST]')
 st.markdown(introduction)
 
 # Initialize chat history
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
 if 'history' not in st.session_state:
     st.session_state['history'] = []
-
-if 'displayed_history' not in st.session_state:
-    st.session_state['displayed_history'] = []
-
-if 'metadatas' not in st.session_state:
-    st.session_state['metadatas'] = []
 
 greeting = """
 Hello! I am the NHLBI BioData Catalyst® Chatbot, also known as BDCBot.
@@ -298,6 +283,31 @@ sample_prompts = [
 # Callback function to update the state
 def handle_click_sample_prompt(prompt):
     st.session_state['sample_prompt_button_pressed'] = prompt
+
+# Display user input
+def display_input(input):
+    st.markdown(input)
+
+# Display a question and response 
+def display_response(response, showBDCSources=False):
+    if response.get('guardrail_response', None):
+       st.write(response["guardrail_response"])
+       return
+    if response.get('predefined_response', None):
+        for predefined in response.get('predefined_response', []):
+            st.write(predefined)
+    if response.get('bdc_response', None):
+        st.write(response["bdc_response"])
+
+        context = response.get("bdc_context", [])      
+        draw_sources(parse_bdc_context(context), showBDCSources)
+    if response.get('dug_response', None):
+        dug_kg = response.get('dug_context', {}).get('dug_kg', None)
+        draw_additional_response(response["dug_response"], "DugBot Response", False, dug_kg)
+    #if response.get('combined_response', None):
+    #    st.write(response["combined_response"])
+    if response.get('code_response', None):
+        st.write(response["code_response"])
 
 with st.chat_message('bdc-assistant'):
     st.markdown(greeting)
@@ -338,142 +348,59 @@ with st.chat_message('bdc-assistant'):
                     args=(prompt,)
                 )
 
-if prompt := (st.chat_input("Ask a question") or st.session_state['sample_prompt_button_pressed']):
-    display_text = ""
-    context = None
-    
-    for i in range(len(st.session_state['displayed_history'])):
-        role, content, sources = st.session_state['displayed_history'][i]
-        bdc_response = ""
-        dug_response = ""
-        if i%2 == 1:
-            j = i//2
-            bdc_response = st.session_state['metadatas'][j].get("bdc_response", "")
-            dug_response = st.session_state['metadatas'][j].get("dug_response", "")
-        with st.chat_message(role):
-            st.markdown(content)
-            if sources:
-                draw_sources(sources, False)
-            if bdc_response and dug_response:
-                #draw_additional_response(bdc_response, "BDC Response", False)
-                draw_additional_response(dug_response, "DugBot Response", False)
-        
+if prompt := (st.chat_input("Ask a question") or st.session_state['sample_prompt_button_pressed']):   
+    for history in st.session_state['history']:        
+        with st.chat_message('using-bdc'):
+            display_input(history.get('input', ''))
+        with st.chat_message('bdc-assistant'):
+            display_response(history.get('response', {}))
+
     with st.chat_message('using-bdc'):
-        st.markdown(prompt)
+        display_input(prompt)
 
     with st.chat_message('bdc-assistant'):
-        response_container = st.empty()
-
         # Add spinner while thinking
-        with st.spinner("Generating response...", show_time=True):
+        with st.spinner("Generating response...", show_time=True):            
+            # Get response from server
+            response = current_chain.invoke({"input": prompt, "chat_history": st.session_state['chat_history']})
 
-            #print("chat_history: ", st.session_state['history'])
+        
+            display_response(response, showBDCSources=True)
             
-            res = current_chain.invoke({"input": prompt, "chat_history": st.session_state['history']})
-            
-            #print("current_chain.invoke: \n", res)                   
-
-            # print keys from res
-            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-            print("input: ", res.get("input", ""))
-            print("res keys: ", res.keys())
-            
-            # Create answer from response
+            # Create answer from response to store for chat history
             answer = ""
+            separator = "\n\n"
 
+            # Combine multiple responses if they exist
+            if response.get('guardrail_response', None):
+                answer += response["guardrail_response"]
+            if response.get('predefined_response', None):
+                for predefined in response.get('predefined_response', []):
+                    answer += separator
+                    answer += predefined
+            if response.get('bdc_response', None):
+                answer += separator
+                answer += response["bdc_response"]
+            if response.get('dug_response', None):
+                answer += separator
+                answer += response["dug_response"]
+            #if response.get('combined_response', None):
+            #    answer += separator
+            #    answer += response["combined_response"]
+            if response.get('code_response', None):
+                answer += separator
+                answer += response["code_response"]
 
-            # XXX             
-            # FIRST: Get working with answer string, as is, ignoring code
-            # SECOND: Refactor to store the full response in session state and create a function that displays here and for previous responses
+    # Store prompts and answer for history to send as context for converstion   
+    st.session_state['chat_history'].extend([(HumanMessage(content=prompt)), (AIMessage(content=answer))])
 
-            if res.get("guardrail_response", None):
-                answer = res["guardrail_response"]
-            else:
-                bdc_response = res.get("bdc_response", "")
-                dug_response = res.get("dug_response", "")
-                if dug_response:
-                    dug_response += "\n\n*Visit [DugBot](https://search-dev.biodatacatalyst.renci.org/chat-v2/) to continue this conversation.*"
-                dug_kg = res.get("dug_context", {}).get("dug_kg", None)
-                combined_response = res.get("combined_response", f"{bdc_response}\n\n{dug_response}")
-                
-                if bdc_response:
-                    answer = bdc_response
-                elif dug_response:
-                    answer = dug_response
-                else:
-                    answer = ""
-                
+    # Store full response for UI display
+    history_object = {
+        "input": prompt,
+        "response": response
+    }   
 
-                
-                # format_predefined_response(predefined_response_list, predefined_context, main_response):
-#                if res.get("prededined_context", {}):
-#                    if res["prededined_context"].get("flag", None) == 'r':
-#                        answer = format_predefined_response(res.get("predefined_response", []), res["prededined_context"], None)
-#                    elif res["prededined_context"].get("flag", None) == 'a':
-#                        answer = format_predefined_response(res.get("predefined_response", []), res["prededined_context"], answer)
-            
-
-            
-            
-            
-            if res.get("predefined_response", None):
-                answer = res.get("predefined_response", ["predefined_response (not found)"])[0]
-            elif res.get("bdc_response", None) and res.get("dug_response", None):
-                #answer = res.get("response", "")
-                # For demo, use bdc response instead of combined response
-                answer = res["bdc_response"]
-            elif res.get("bdc_response", None):
-                answer = res["bdc_response"]
-            elif res.get("dug_response", None):
-                answer = res["dug_response"] 
-            
-
-            
-            
-            context = res.get("bdc_context", [])
-            
-            for i, doc in enumerate(context):
-                context[i] = doc.dict()
-            
-            
-            print("bot answer: ", answer)
-            
-            display_answer = answer
-            # if res.get("flag", None) == 'a':
-            #     display_answer += "\n\n" + res.get("predefined_response", "predefined_response (not found)")
-            #if res.get("dug_response", None):
-            #    display_answer += "\n\nVisit the [DUG Bot](https://search-dev.biodatacatalyst.renci.org/chat-v2/) for more information."
-            
-            
-            # bdc_response = res.get("bdc_response", "")
-            # dug_response = res.get("dug_response", "")
-            # dug_kg = res.get("dug_kg")
-            # print("flag: ", res["flag"])
-            
-            # display_text += answer
-
-            # if res.get("dug_response", None):
-            #     dug_response += "\n\n*Visit [DugBot](https://search-dev.biodatacatalyst.renci.org/chat-v2/) to continue this conversation.*"
-
-            sources = parse_context(context)
-
-
-            response_container.markdown(answer, unsafe_allow_html=True)
-
-            draw_sources(sources, False)
-
-            if bdc_response and dug_response:
-                print(dug_response)
-                res.get("dug_context", {})
-
-                #draw_additional_response(bdc_response, "BDC Response", False)
-                draw_additional_response(dug_response, "DugBot Response", False, dug_kg)
-    
-    # st.session_state['history'].extend([dumps(HumanMessage(content=prompt)), dumps(AIMessage(content=answer))])
-    st.session_state['history'].extend([(HumanMessage(content=prompt)), (AIMessage(content=answer))])
-    st.session_state['displayed_history'].append(('using-bdc', prompt, None))
-    st.session_state['displayed_history'].append(('bdc-assistant', answer, sources))
-    st.session_state['metadatas'].append(res)
+    st.session_state['history'].append(history_object)
 
 # Disclaimer at bottom right
 st.markdown(
